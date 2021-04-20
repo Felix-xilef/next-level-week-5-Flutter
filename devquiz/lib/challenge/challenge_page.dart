f62b15e1da579cd8de04c711d623f26ef3553224
Felix <felixpb@yahoo.com.br>
2021-04-20 19:23:02 -0300

    Aula 2
    Conteúdo da aula 2 - dia 20/04/2021

---
+++ devquiz/lib/challenge/challenge_page.dart
@@ -0,0 +1,26 @@
+import 'package:devquiz/challenge/widget/question_indicator/question_indicator.dart';
+import 'package:devquiz/challenge/widget/quiz/quiz_widget.dart';
+import 'package:flutter/material.dart';
+
+class ChallengePage extends StatefulWidget {
+  @override
+  _ChallengePageState createState() => _ChallengePageState();
+}
+
+class _ChallengePageState extends State<ChallengePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(80),
+        child: SafeArea(
+          top: true,
+          child: QuestionIndicatorWidget(),
+        ),
+      ),
+      body: QuizWidget(
+        title: "O que o Flutter faz em sua totalidade?",
+      ),
+    );
+  }
+}
---
+++ devquiz/lib/challenge/widget/quiz/quiz_widget.dart
@@ -0,0 +1,44 @@
+import 'package:devquiz/challenge/widget/answer/answer_widget.dart';
+import 'package:devquiz/core/app_text_styles.dart';
+import 'package:flutter/material.dart';
+
+class QuizWidget extends StatelessWidget {
+  final String title;
+
+  const QuizWidget({Key? key, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Text(
+              title,
+              style: AppTextStyles.heading,
+              textAlign: TextAlign.center,
+            ),
+          ),
+          SizedBox(
+            height: 24,
+          ),
+          AnswerWidget(
+            title: "Possibilita a criação de aplicativos compilados nativamente",
+          ),
+          AnswerWidget(
+            title: "Possibilita a criação de aplicativos compilados nativamente",
+            isRight: true,
+            isSelected: true,
+          ),
+          AnswerWidget(
+            title: "Possibilita a criação de aplicativos compilados nativamente",
+          ),
+          AnswerWidget(
+            title: "Possibilita a criação de aplicativos compilados nativamente",
+          ),
+        ],
+      ),
+    );
+  }
+}
